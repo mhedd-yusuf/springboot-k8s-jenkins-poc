f40f2ad0b98808061bb1ca2fd9c31c2181d6ae1d
mhedd-yusuf <eltumerabe@gmail.com>
2025-08-26 13:45:39 +0100

    first commit

---
+++ src/main/java/com/example/springbootk8sjenkinspoc/SpringbootK8sJenkinsPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootk8sjenkinspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootK8sJenkinsPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootK8sJenkinsPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootk8sjenkinspoc/SpringbootK8sJenkinsPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootk8sjenkinspoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootK8sJenkinsPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
